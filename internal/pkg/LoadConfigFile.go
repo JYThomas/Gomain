5b3f9d4b11a57fbf4b0cc70da955ce0e58806fa3
JYThomas <1741686609@qq.com>
2024-09-19 23:19:10 +0800

    Update load Config file function

--- internal/pkg/LoadConfigFile.go
+++ internal/pkg/LoadConfigFile.go
@@ -2,26 +2,62 @@ package pkg
 
 import (
 	"errors"
+	"fmt"
+	"os"
 	"path/filepath"
 
 	"gopkg.in/ini.v1"
 )
 
+// 从当前工作目录向上查找直到找到Gomain目录
+func findGomainDir(startPath string) (string, error) {
+	for {
+		gomainDir := filepath.Join(startPath, "Gomain")
+
+		if _, err := os.Stat(gomainDir); !os.IsNotExist(err) {
+			return gomainDir, nil
+		}
+
+		// 获取父目录
+		parentDir := filepath.Dir(startPath)
+		if parentDir == startPath { // 达到根目录
+			return "", errors.New("Error: No Gomain Dir")
+		}
+
+		startPath = parentDir
+	}
+}
+
 // 加载配置文件
 func LoadConfig() (*ini.File, error) {
-	// 获取当前文件夹所在路径
-	basePath, err := filepath.Abs(filepath.Dir("."))
+	// 获取当前工作目录
+	startPath, err := filepath.Abs(".")
 	if err != nil {
-		return nil, errors.New("Failed to determine current directory")
+		return nil, errors.New("Load Config File Error: fail to find current File")
 	}
 
-	// 拼接配置文件路径
-	configPath := filepath.Join(basePath, "../../../Config.ini")
+	// 查找Gomain目录
+	gomainDir, err := findGomainDir(startPath)
+	if err != nil {
+		return nil, fmt.Errorf("Load Config File Error: Fail to find Gomain dir: %v", err)
+	}
+
+	// 配置文件相对路径
+	configRelPath := "internal/Config.ini"
+	configPath := filepath.Join(gomainDir, configRelPath)
+
+	fmt.Println(configPath)
+
+	// 检查配置文件是否存在
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		return nil, fmt.Errorf("Load Config File Error: Config File Not Exist: %s", configPath)
+	}
 
 	// 加载配置文件
 	cfg, err := ini.Load(configPath)
 	if err != nil {
-		return nil, errors.New("Module RAPIDDNS: Config Load Error")
+		return nil, fmt.Errorf("Load Config File Error: %v", err)
 	}
-	return cfg, err
+
+	return cfg, nil
 }
